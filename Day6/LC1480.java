bcb656ac0e4d4557d9e83ba1016028e4c75ae194
Prawin <itz.mpwin07@gmail.com>
2026-01-29 23:04:36 +0530

    Day 6

---
+++ Day6/LC1480.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class LC1480 {
+    public static int[] runningSum(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] += nums[i - 1];
+        }
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int[] res = runningSum(nums);
+        for (int x : res) {
+            System.out.print(x + " ");
+        }
+    }
+}
---
+++ Day6/LC1491.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class LC1491 {
+    public static double average(int[] salary) {
+        int sum = 0, min = salary[0], max = salary[0];
+
+        for (int s : salary) {
+            sum += s;
+            if (s < min) min = s;
+            if (s > max) max = s;
+        }
+
+        return (double)(sum - min - max) / (salary.length - 2);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] salary = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            salary[i] = sc.nextInt();
+        }
+
+        System.out.println(average(salary));
+    }
+}
