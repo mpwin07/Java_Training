7956a32da340ce7b978bfc0e6d6fc0f1c8874fce
Prawin <itz.mpwin07@gmail.com>
2026-02-10 22:19:57 +0530

    Day 16

--- Day15/LC355.java
+++ Day15/LC355.java
@@ -1,69 +1,75 @@
-// Twitter design
-
-
 import java.util.*;
 
 class Twitter {
 
-    private static class Tweet {
-        int id;
-        int time;
-        Tweet(int id, int time) {
-            this.id = id;
-            this.time = time;
-        }
-    }
-
-    private Map<Integer, Set<Integer>> followMap;
-    private Map<Integer, List<Tweet>> tweetMap;
-    private int time;
+    private List<int[]> tweets;                 // [userId, tweetId]
+    private Map<Integer, Set<Integer>> follow;  // follower → followees
 
     public Twitter() {
-        followMap = new HashMap<>();
-        tweetMap = new HashMap<>();
-        time = 0;
+        tweets = new ArrayList<>();
+        follow = new HashMap<>();
     }
 
     public void postTweet(int userId, int tweetId) {
-        tweetMap.putIfAbsent(userId, new ArrayList<>());
-        tweetMap.get(userId).add(new Tweet(tweetId, time++));
+        tweets.add(new int[]{userId, tweetId});
     }
 
     public List<Integer> getNewsFeed(int userId) {
-        PriorityQueue<Tweet> pq = new PriorityQueue<>((a, b) -> b.time - a.time);
+        List<Integer> res = new ArrayList<>();
+        Set<Integer> fset = follow.getOrDefault(userId, new HashSet<>());
+        fset.add(userId);
 
-        // User’s own tweets
-        if (tweetMap.containsKey(userId)) {
-            pq.addAll(tweetMap.get(userId));
-        }
-
-        // Followees' tweets
-        if (followMap.containsKey(userId)) {
-            for (int f : followMap.get(userId)) {
-                if (tweetMap.containsKey(f)) {
-                    pq.addAll(tweetMap.get(f));
-                }
+        for (int i = tweets.size() - 1; i >= 0 && res.size() < 10; i--) {
+            int[] t = tweets.get(i);
+            if (fset.contains(t[0])) {
+                res.add(t[1]);
             }
         }
-
-        List<Integer> feed = new ArrayList<>();
-        int count = 0;
-        while (!pq.isEmpty() && count < 10) {
-            feed.add(pq.poll().id);
-            count++;
-        }
-
-        return feed;
+        return res;
     }
 
     public void follow(int followerId, int followeeId) {
-        followMap.putIfAbsent(followerId, new HashSet<>());
-        followMap.get(followerId).add(followeeId);
+        if (followerId == followeeId) return;
+        follow.computeIfAbsent(followerId, k -> new HashSet<>()).add(followeeId);
     }
 
     public void unfollow(int followerId, int followeeId) {
-        if (followMap.containsKey(followerId)) {
-            followMap.get(followerId).remove(followeeId);
+        if (follow.containsKey(followerId))
+            follow.get(followerId).remove(followeeId);
+    }
+}
+
+public class LC355 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Twitter tw = new Twitter();
+
+        int q = sc.nextInt();   // number of operations
+        while (q-- > 0) {
+            String op = sc.next();
+
+            if (op.equals("post")) {
+                int u = sc.nextInt();
+                int t = sc.nextInt();
+                tw.postTweet(u, t);
+            }
+            else if (op.equals("follow")) {
+                int f1 = sc.nextInt();
+                int f2 = sc.nextInt();
+                tw.follow(f1, f2);
+            }
+            else if (op.equals("unfollow")) {
+                int f1 = sc.nextInt();
+                int f2 = sc.nextInt();
+                tw.unfollow(f1, f2);
+            }
+            else if (op.equals("get")) {
+                int u = sc.nextInt();
+                List<Integer> feed = tw.getNewsFeed(u);
+                for (int x : feed) System.out.print(x + " ");
+                System.out.println();
+            }
         }
+        sc.close();
     }
 }
