41b190606074894c9ff98316cbb07098943ed5cc
Prawin <itz.mpwin07@gmail.com>
2026-03-24 23:46:28 +0530

    Arrays

---
+++ Arrays/LC189.java
@@ -0,0 +1,22 @@
+// 189. Rotate Array
+// Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k%n;
+        rev(nums,0,n-1);
+        rev(nums,0,k-1);
+        rev(nums,k,n-1);
+                
+    }
+    private void rev(int[] nums, int l, int r){
+        while(l<r){
+            int temp = nums[l];
+            nums[l] = nums[r];
+            nums[r] = temp;
+            l++;
+            r--;
+        }
+    }
+}
---
+++ Arrays/LC238.java
@@ -0,0 +1,24 @@
+// 238. Product of Array Except Self
+// Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+// The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+// You must write an algorithm that runs in O(n) time and without using the division operation.
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int [] res = new int[n];
+
+        int prefix = 1;
+        for(int  i = 0; i < n ; i++){
+            res[i] = prefix;
+            prefix *= nums[i];
+        }
+
+        int postfix = 1;
+        for(int i = n-1; i>= 0 ; i--){
+            res[i] *= postfix;
+            postfix *= nums[i];
+        }
+        return res;        
+    }
+}
