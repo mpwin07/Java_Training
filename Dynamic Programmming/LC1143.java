ddc391793f826e299beef753ab2516e51ab7ff62
Prawin <itz.mpwin07@gmail.com>
2026-04-01 23:41:00 +0530

    DP

--- Dynamic Programmming/LC1143.java
+++ Dynamic Programmming/LC1143.java
@@ -7,3 +7,23 @@
 // For example, "ace" is a subsequence of "abcde".
 // A common subsequence of two strings is a subsequence that is common to both strings.
 
+
+class Solution{
+    public int longestChSubsequence(String t1, String t2){
+        int n1 = t1.length();
+        int n2 = t2.length();
+
+        int[][] dp = new int [n1+1][n2+1];
+        for(int i=n1-1; i>=0; i--){
+            for(int j=n2-1; j>=0; j--){
+                if(t1.charAt(i) == t2.charAt(j)){
+                    dp[i][j] = 1 + dp[i+1][j+1];
+                }
+                else{
+                    dp[i][j] = Math.max(dp[i][j+1] , dp[i+1][j]);
+                }
+            }
+        }
+        return dp[0][0];
+    }
+}
--- Dynamic Programmming/LC139.java
+++ Dynamic Programmming/LC139.java
@@ -3,23 +3,3 @@
 // Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
 
 // Note that the same word in the dictionary may be reused multiple times in the segmentation.
-
-class Solution{
-    public int longestChSubsequence(String t1, String t2){
-        int n1 = t1.length();
-        int n2 = t2.length();
-
-        int[][] dp = new int [n1+1][n2+1];
-        for(int i=n1-1; i>=0; i--){
-            for(int j=n2-1; j>=0; j--){
-                if(t1.charAt(i) == t2.charAt(j)){
-                    dp[i][j] = 1 + dp[i+1][j+1];
-                }
-                else{
-                    dp[i][j] = Math.max(dp[i][j+1] , dp[i+1][j]);
-                }
-            }
-        }
-        return dp[0][0];
-    }
-}
